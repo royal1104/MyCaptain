b01008f185157f85a1ad556f9c3c99449fdb9567
royal1104 <78022971+royal1104@users.noreply.github.com>
2021-06-18 09:02:17 +0530

    Update frequency.py

--- frequency.py
+++ frequency.py
@@ -1,27 +1,15 @@
-# W= input('Please enter a string ')
-# def most_frequent(string):
-#     d = dict()
-#     for key in string:
-#         if key not in d:
-#             d[key] = 1
-#         else:
-#             d[key] += 1
-#     return d
-#
-# print (most_frequent(W))
+W= input('Please enter a string ')
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print (most_frequent(W))
 
 
-# input_string = input(print("Enter a string"))
-#
-# frequencies = {}
-#
-# for char in input_string:
-#     if char in frequencies:
-#         frequencies[char] += 1
-#     else:
-#         frequencies[char] = 1
-#
-# # Show Output
-# print("Per char frequency in '{}' is :\n {}".format(input_string, str(frequencies)))
 
 
